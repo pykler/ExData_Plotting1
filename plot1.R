3de652e450e74d6c6b17a7e7ae56c839f9229c21
Hatem Nassrat <hnassrat@gmail.com>
2015-07-11 15:21:18 -0300

    Adding plot1.

---
+++ plot1.R
@@ -0,0 +1,18 @@
+setwd('/Users/hatem/projects/exdata/ExData_Plotting1')
+source('loadExData.r')
+
+# Load data
+data <- loadExData()
+
+# Open the png device
+# Using values to make identical to the plot in figures/*
+png('plot1.png', bg="transparent", width = 504, height = 504, units = "px")
+
+# Generate the plot
+hist(
+  data$Global_active_power, col="red",
+  main="Global Active Power", xlab="Global Active Power (kilowatts)"
+)
+
+# Save the plot
+dev.off()
