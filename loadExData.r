5f5116ee5cbfac1f5f1bb33e68c141c8fed7087a
Hatem Nassrat <hnassrat@gmail.com>
2015-07-11 15:20:54 -0300

    Adding date and time conversions + proper headers.

--- loadExData.r
+++ loadExData.r
@@ -1,11 +1,13 @@
 setwd('/Users/hatem/projects/exdata/ExData_Plotting1')
 
 loadExData <- function(){
-    data <- read.csv2(
-        'data/household_power_consumption.txt', na.strings="?",
-        skip=66636, nrows=2880
+    fn <- 'data/household_power_consumption.txt'
+    first <- read.csv(fn, sep = ";", nrows=1)
+    data <- read.csv(
+      fn, sep = ";", na.strings="?", skip=66637, nrows=2880,
+      header=FALSE, col.names=colnames(first)
     )
-    print(data[1,1])
-    print(data[1,2])
+    data$Time <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S", tz="GMT")
+    data$Date <- as.Date(data$Date , "%d/%m/%Y")
     return(data)
 }
